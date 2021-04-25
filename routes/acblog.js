44ebd8ef9463a3905b333db5425309370e2621b6
shadowlerone <andrae.lewis@gmail.com>
2021-04-25 14:51:24 -0400

    Animal crossing progress

--- routes/acblog.js
+++ routes/acblog.js
@@ -2,6 +2,7 @@ var express = require('express');
 const fs = require('fs');
 var router = express.Router();
 var lipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum nibh ac placerat placerat. Ut dignissim iaculis risus et maximus. Integer fringilla lectus nisl, in mattis justo suscipit vel. Duis malesuada pellentesque volutpat. Vestibulum malesuada vel risus non convallis. Nam at nibh et lectus auctor hendrerit eu sit amet lacus. Suspendisse molestie massa non scelerisque sagittis. Duis interdum pretium efficitur. Praesent ac nunc vitae velit sodales porttitor nec quis neque. Donec luctus turpis ut scelerisque tristique. Donec ultrices egestas dignissim. Curabitur quis turpis aliquam, pharetra mi ut, sagittis mi. Aliquam egestas velit eu tellus vulputate, quis gravida orci ultrices. Nullam eros dui, laoreet ac semper a, volutpat a diam.`
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 getJson = function (fp) {
 	let rawdata = fs.readFileSync(fp);
 	return JSON.parse(rawdata);
@@ -14,9 +15,10 @@ router.get('/', function (req, res, next){
 
 router.get('/feed', function (req, res, next) {
 	let characters = getJson('routes/animalcrossing/profiles.json');
-	let posts = getJson('routes/animalcrossing/posts.json').map(p => {
+	let posts = getJson('routes/animalcrossing/posts.json').filter(p => new Date(p.date) < new Date()).slice().sort((a, b) => { i = new Date(a.date); j = new Date(b.date); j - i }).map(p => {
 		if (p.content=="lorem"){p.content = lipsum};
 		p.author_img = characters[p.author].image;
+		p.date = new Date(p.date).toLocaleDateString(undefined, options);
 		return p;
 	});
 
@@ -39,8 +41,9 @@ router.get('/profile/:char', function (req, res, next) {
 
 });
 
-router.get('/profile', function (req, res, next) {
-	res.render("animalcrossing/profiles");
+router.get('/profiles', function (req, res, next) {
+	let characters = getJson('routes/animalcrossing/profiles.json');
+	res.render("animalcrossing/profiles", {characters : characters});
 });
 
 module.exports = router;
